eb25317009202e70650ff9e442572b8b36e31d95
ajvolin <ajvolin@users.noreply.github.com>
2021-02-02 14:18:27 -0500

    Update run.sh
    Add startup delay option

--- run.sh
+++ run.sh
@@ -5,5 +5,12 @@ if [ ! -x /channels-dvr/latest/channels-dvr ]; then
   curl -f -s https://getchannels.com/dvr/setup.sh | DOWNLOAD_ONLY=1 sh
 fi
 cd /channels-dvr/data
-echo Running Channels DVR..
+
+if [[ -z "${STARTUP_DELAY}" ]]; then
+  echo Running Channels DVR..
+else
+  echo "Delaying Channels DVR startup for $STARTUP_DELAY"
+  sleep $STARTUP_DELAY
+  echo Running Channels DVR..
+fi
 exec ../latest/channels-dvr >> channels-dvr.log 2>&1
